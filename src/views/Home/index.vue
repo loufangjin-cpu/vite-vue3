ad56529ee265ec51b123b4957f570549f2faf75d
loufangjin14052 <loufangjin14052@ipalfish.com>
2021-03-02 14:00:29 +0800

    feat: first

---
+++ src/views/Home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div @click="goLogin">goLogin</div>
+  <div>3</div>
+  <div>2</div>
+</template>
+<script lang="ts" setup="props">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goLogin = () => {
+  router.push('/login')
+}
+</script>
+<style scoped></style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>1</div>
+  <div>3</div>
+  <div>2</div>
+</template>
+<script lang='ts' setup='props'>
+
+</script>
+<style scoped>
+
+</style>
