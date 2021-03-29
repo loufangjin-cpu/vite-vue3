c66a7d558979e35b46295cc2cc66d699f07921f5
loufangjin14052 <loufangjin14052@ipalfish.com>
2021-03-29 15:25:20 +0800

    feat: own

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <RouterView />
-  <!-- <HelloWorld /> -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-// import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    // HelloWorld
   }
 })
 </script>
