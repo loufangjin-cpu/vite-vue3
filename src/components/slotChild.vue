3d0d97c08047438bdddf17231f44dcd0353e08a4
loufangjin14052 <loufangjin14052@ipalfish.com>
2021-03-31 11:03:55 +0800

    feat: slots

---
+++ src/components/slotChild.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- <div class="header">
+      具名插槽
+      header:
+      <slot name="header">里面内容不展示</slot>
+    </div>
+    <div class="content">
+      content:
+      <slot></slot>
+    </div>
+    <div class="footer">
+      footer:
+      <slot name="footer"></slot>
+    </div> -->
+    <!-- 作用域插槽 ： 组件可能被很多地方调用，在很多时候列表循环的样式不是<child>所控制的，而是外部控制的，外部来指定如何修改如何渲染 -->
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <slot :item='item'> </slot>
+        <!-- {{item.name}} -->
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { defineComponent, reactive, toRefs } from 'vue'
+
+export default defineComponent({
+  name:'slotChild',
+  setup() {
+    let list = reactive([
+        { name: 'zs'  },
+        { name: 'ls'  },
+        { name: 'ww'  }
+    ])
+    return {
+      list
+    }
+  },
+})
+</script>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,18 +1,79 @@
 <template>
-  <h1>登录页</h1>
-  vuex:{{ $store.state.moduleA.num }}
   <div>
-    <van-button type="primary" @click="goLogin">跳转到首页</van-button>
+    <h1>登录页</h1>
+    vuex:{{ $store.state.moduleA.num }}
+    <div>
+      <van-button type="primary" @click="goLogin">跳转到首页</van-button>
+    </div>
+    <!-- <slot-child>
+      <template #header>
+        <h1>Here might be a page title</h1>
+      </template>
+
+      <template #default>
+        <p>A paragraph for the main content.</p>
+        <p>And another one.</p>
+      </template>
+
+      <template #footer>
+        <p>Here's some contact info</p>
+      </template>
+    </slot-child> -->
+
+    <!-- 1、解构赋值 -->
+    <!-- <slot-child #default="{ item, index }">
+      {{item.name}} --- {{index}}
+    </slot-child> -->
+
+    <!-- 2、template形式作用域 -->
+    <!-- <slot-child>
+      <template v-slot:default="slotProps">
+        <i class="fas fa-check"></i>
+        <span class="green">???{{ slotProps.item.name }}</span>
+      </template>
+    </slot-child> -->
+
+    <!-- 3、template形式作用域, vue3形式 -->
+    <!-- <slot-child>
+      <template #default="slotProps">
+        <i class="fas fa-check"></i>
+        <span class="green"> ///{{ slotProps.item.name }}</span>
+      </template>
+    </slot-child> -->
+
+    <!-- <child>组件可能被很多地方调用，在很多时候列表循环的样式不是<child>所控制的，而是外部控制的 -->
+    <!-- <slot-child></slot-child>
+    <slot-child></slot-child> -->
   </div>
 </template>
-<script lang="ts" setup="props">
-import { useRouter } from "vue-router";
+<script>
+import { defineComponent, reactive, toRefs } from 'vue'
+import { useRouter } from 'vue-router'
 import qs from 'query-string'
+import SlotChild from '@/components/SlotChild.vue'
 
-const router = useRouter();
+const router = useRouter()
 const params = qs.parse(window.location.search)
 console.log('params', params)
-const goLogin = () => {
-  router.push("/");
-};
+export default defineComponent({
+  name: 'Login',
+  components: {
+    SlotChild
+  },
+  setup() {
+    SlotChild
+    const state = reactive({
+      slotProps: {
+        item: 'hello'
+      }
+    })
+    const goLogin = () => {
+      router.push('/')
+    }
+    return {
+      goLogin,
+      ...toRefs(state)
+    }
+  }
+})
 </script>
